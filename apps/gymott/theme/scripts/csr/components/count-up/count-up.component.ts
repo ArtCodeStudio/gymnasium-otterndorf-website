ba743d784176279929c172f29de45a4248fb73c7
Pascal Garber <pascal@artandcode.studio>
2021-02-24 20:19:09 +0000

    Upgrade dependencies and cleanups

--- apps/gymott/theme/scripts/csr/components/count-up/count-up.component.ts
+++ apps/gymott/theme/scripts/csr/components/count-up/count-up.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@ribajs/core";
 import { CountUp } from "countup.js";
 import { isInViewport } from "@ribajs/utils/src/dom";
+import { throttle, debounce } from "@ribajs/utils/src/control";
 
 export interface Scope {
   target: number;
@@ -10,6 +11,7 @@ export class CountUpComponent extends Component {
   public static tagName = "count-up";
   public _debug = false;
   protected autobind = true;
+  protected countUp: CountUp | null = null;
 
   scope: Scope = {
     target: 0,
@@ -27,16 +29,50 @@ export class CountUpComponent extends Component {
     super();
   }
 
+  protected checkViewport() {
+    console.debug("checkViewport");
+    if (isInViewport(this)) {
+      this.countUp?.start();
+    }
+  }
+
+  protected addEventListeners() {
+    window.addEventListener("scroll", this.onScroll.bind(this), {
+      passive: true,
+    });
+    window.addEventListener("resize", this.onResize.bind(this), {
+      passive: true,
+    });
+  }
+
+  protected removeEventListeners() {
+    window.removeEventListener("scroll", this.onScroll.bind(this));
+    window.removeEventListener("resize", this.onResize.bind(this));
+  }
+
+  protected onResize() {
+    throttle(() => {
+      this.checkViewport();
+    }, 500)();
+  }
+
+  protected onScroll() {
+    throttle(() => {
+      this.checkViewport();
+    }, 500)();
+  }
+
+  protected async beforeBind() {
+    await super.beforeBind();
+    this.countUp = new CountUp(this, this.scope.target);
+  }
+
   protected async afterBind() {
+    this.addEventListeners();
+    this.checkViewport();
+
+    // This must be called on the end of this function
     await super.afterBind();
-    const countUp = new CountUp(this, this.scope.target);
-    const listener = () => {
-      if (isInViewport(this)) {
-        countUp.start();
-        window.removeEventListener("scroll", listener);
-      }
-    };
-    window.addEventListener("scroll", listener);
   }
 
   protected connectedCallback() {
--- apps/gymott/theme/scripts/csr/services/graphql.ts
+++ apps/gymott/theme/scripts/csr/services/graphql.ts
@@ -3,25 +3,26 @@ import { GraphQLClient as _GraphQLClient } from "graphql-request";
 import authMutation from "../../../graphql/mutations/auth.gql";
 
 export class GraphQLClient extends _GraphQLClient {
-
   protected static instance: GraphQLClient;
 
-  
   protected constructor(url: string, options?: RequestInit) {
     super(url, options);
   }
 
   public static getInstance() {
-    if(GraphQLClient.instance) {
+    if (GraphQLClient.instance) {
       return GraphQLClient.instance;
     }
     // TODO get STRAPI_EXTERN_URL env over config api
-    GraphQLClient.instance = new GraphQLClient(window.ssr?.env?.STRAPI_EXTERN_URL ? window.ssr?.env?.STRAPI_INTERN_URL + "/graphql" : '');
+    GraphQLClient.instance = new GraphQLClient(
+      window.ssr?.env?.STRAPI_EXTERN_URL
+        ? window.ssr?.env?.STRAPI_INTERN_URL + "/graphql"
+        : ""
+    );
     return GraphQLClient.instance;
   }
 
   async auth(email: string, password: string) {
-    return this.request(authMutation, {email, password});
+    return this.request(authMutation, { email, password });
   }
-
 }
