db8b776c1e63a5a5a81d69c5976d049233a1eaa3
Pascal Garber <pascal@artandcode.studio>
2021-03-03 17:06:23 +0000

    [Theme] Convert pug partials to riba components

--- apps/gymott/theme/scripts/ssr/components/index.ts
+++ apps/gymott/theme/scripts/ssr/components/index.ts
@@ -1,6 +1,8 @@
 export { GyLinkListComponent } from "./gy-link-list/gy-link-list.component";
+export { GyNavbarComponent } from "./gy-navbar/gy-navbar.component";
 export { GySectionFactsComponent } from "./gy-section-facts/gy-section-facts.component";
 export { GySectionImageComponent } from "./gy-section-image/gy-section-image.component";
 export { GySectionSlideshowComponent } from "./gy-section-slideshow/gy-section-slideshow.component";
 export { GySectionTextComponent } from "./gy-section-text/gy-section-text.component";
 export { GySectionsHomeComponent } from "./gy-sections-home/gy-sections-home.component";
+export { GySidebarComponent } from "./gy-sidebar/gy-sidebar.component";
