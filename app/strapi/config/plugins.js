1f1c55f31664ec8cde9d5acaa58de4ee147a8992
Pascal Garber <pascal@artandcode.studio>
2021-06-14 11:59:06 +0000

    [Strapi] GraphQL: Changed max depth from 7 to 10

---
+++ app/strapi/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 10,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+};
