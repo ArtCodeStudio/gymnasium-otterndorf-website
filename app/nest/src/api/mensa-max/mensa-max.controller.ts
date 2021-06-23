7aafd5f7c178266cfea573a85697132efee47cb9
Pascal Garber <pascal@artandcode.studio>
2021-06-23 08:16:52 +0000

    [Nest] Parse MensaMax html

--- app/nest/src/api/mensa-max/mensa-max.controller.ts
+++ app/nest/src/api/mensa-max/mensa-max.controller.ts
@@ -1,24 +1,36 @@
-import { Controller, Get, Res, Query } from '@nestjs/common';
+import { Controller, Get, Res, Param } from '@nestjs/common';
 import { Response } from 'express';
 import { MensaMaxService } from './mensa-max.service';
-import { ApiQuery, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('mensa-max')
-@Controller('api/mensa-max')
+@Controller('api/mensa-max/:p/:e')
 export class MensaMaxController {
   constructor(readonly mensaMax: MensaMaxService) {}
 
-  @ApiQuery({
-    name: 'key',
+  @ApiParam({
+    name: 'p',
     description:
-      'The calendar key, if no key is specified, the default public calendar is used.',
-    required: false,
+      'The "P" query parameter for the original MensaMax URL, e.g. "CUX000"',
+    type: String,
+  })
+  @ApiParam({
+    name: 'e',
+    description:
+      'The "E" query parameter for the original MensaMax URL, e.g. "SZO"',
     type: String,
   })
   @Get()
-  @ApiOperation({ summary: 'Get the dishes of the current week' })
-  async get(@Res() res: Response) {
-    const html = await this.mensaMax.getMensaTable();
-    res.json({ html });
+  @ApiOperation({
+    summary:
+      'Get the dishes of the current week, e.g. "/api/mensa-max/CUX000/SZO"',
+  })
+  async get(
+    @Res() res: Response,
+    @Param('p') p: string,
+    @Param('e') e: string,
+  ) {
+    const result = await this.mensaMax.getData(p, e);
+    res.json(result);
   }
 }
--- app/nest/src/api/mensa-max/mensa-max.service.ts
+++ app/nest/src/api/mensa-max/mensa-max.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { Builder, By, until } from 'selenium-webdriver';
 import * as firefox from 'selenium-webdriver/firefox';
 
-import { minify } from 'html-minifier';
+import { minify as minifier } from 'html-minifier';
+import cheerio from 'cheerio';
 
 require('geckodriver');
 
 @Injectable()
 export class MensaMaxService {
-  async getMensaTable() {
+  async getMensaTable(url: string, minify = false) {
     const options = new firefox.Options();
     options.headless();
 
@@ -17,30 +18,71 @@ export class MensaMaxService {
       .forBrowser('firefox')
       .build();
     try {
-      await driver.get(
-        'https://login.mensaonline.de/LOGINPLAN.ASPX?P=CUX000&E=SZO',
-      );
+      await driver.get(url);
       const outerTableLocator = By.className('Outertable');
       await driver.wait(until.elementsLocated(outerTableLocator), 5000);
       const outerTableEl = await driver.findElement(outerTableLocator);
       const html = await outerTableEl.getAttribute('outerHTML');
       // const html = await driver.getPageSource();
-      return minify(html, {
-        collapseWhitespace: true,
-        collapseBooleanAttributes: true,
-        collapseInlineTagWhitespace: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeEmptyElements: true,
-        removeEmptyAttributes: true,
-        removeOptionalTags: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        removeTagWhitespace: true,
-      });
+      if (minify) {
+        return minifier(html, {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          collapseInlineTagWhitespace: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyElements: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          removeTagWhitespace: true,
+        });
+      } else {
+        return html;
+      }
     } finally {
       await driver.quit();
     }
+  }
+
+  async getData(p: string, e: string) {
+    const url = `https://login.mensaonline.de/LOGINPLAN.ASPX?P=${p}&E=${e}`;
+    console.debug('url', url);
+    const html = await this.getMensaTable(url, true);
+    const $dom = cheerio.load(html);
+
+    const timeRange = $dom('#lblWoche').text();
+
+    const $mainTable = $dom('#tblMain');
+
+    const $header = $dom('.tdHeader > th', $mainTable);
+
+    const header: string[] = [];
+    const foods: string[][] = [[], []];
+
+    for (const head of $header) {
+      header.push($dom(head).text());
+    }
+
+    const $foodsRow1 = $dom('.tdFooter2 > td', $mainTable);
+    const $foodsRow2 = $dom('.tdFooter > td', $mainTable);
+
+    for (const food of $foodsRow1) {
+      foods[0].push($dom(food).html());
+    }
+
+    for (const food of $foodsRow2) {
+      foods[1].push($dom(food).html());
+    }
+
+    return {
+      url,
+      html,
+      timeRange,
+      header,
+      foods,
+    };
   }
 }
