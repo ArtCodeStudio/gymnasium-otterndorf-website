e5886920881b8a0d7d8e3e23c40c6d060a66b012
Pascal Garber <pascal@artandcode.studio>
2021-07-06 13:21:14 +0000

    [Theme] Improved gallery page style

--- app/theme/scripts/csr/pages/index.ts
+++ app/theme/scripts/csr/pages/index.ts
@@ -2,6 +2,7 @@ export { NotFoundPageComponent } from "./404/404.component";
 export { ErrorPageComponent } from "./500/500.component";
 export { BlogPageComponent } from "./blog/blog.component";
 export { BlogEntryPageComponent } from "./blog-entry/blog-entry.component";
+export { GalleryPageComponent } from "./gallery/gallery.component";
 export { IndexPageComponent } from "./index/index.component";
 export { MediaCenterPageComponent } from "./media-center/media-center.component";
 export { PagesPageComponent } from "./pages/pages.component";
