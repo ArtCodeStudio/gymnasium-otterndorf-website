d9e3f815ba86d4eb379f06a1038c504dec133729
Pascal Garber <pascal@artandcode.studio>
2021-07-19 11:56:24 +0000

    [Theme] Credits Open Graph tag

--- app/theme/scripts/ssr/pages/credits/credits.component.ts
+++ app/theme/scripts/ssr/pages/credits/credits.component.ts
@@ -1,11 +1,13 @@
 import { PageComponent } from "@ribajs/ssr";
 import pugTemplate from "./credits.component.pug";
-import { replaceBodyPageClass } from "../../../common";
+import { replaceBodyPageClass, nestFormatter } from "../../../common";
+import { OpenGraphService } from "../../services";
 
 export class CreditsPageComponent extends PageComponent {
   public static tagName = "credits-page";
   public _debug = false;
   protected autobind = true;
+  protected openGraph = OpenGraphService.getInstance();
 
   scope = {};
 
@@ -29,6 +31,21 @@ export class CreditsPageComponent extends PageComponent {
 
   protected async beforeBind() {
     this.head.title = "Credits, Quellcode und API";
+    await this.openGraph.setWebsite({
+      title: this.head.title,
+      description:
+        "Informationen darüber wer bei der Seite mitgewirkt hat, wie du an den Quellcode kommst und wie du die API verwenden kannst.",
+      image: [
+        {
+          url: nestFormatter.read("images/credits.png"),
+          width: 1200,
+          height: 1200,
+          alt: "API Image",
+        },
+      ],
+      url: nestFormatter.read("credits"),
+    });
+
     await super.beforeBind();
   }
 
