6783985a231c6a889c120077e20373bef2ef298a
Pascal Garber <pascal@artandcode.studio>
2021-11-12 12:01:37 +0000

    [Theme] Moved IDBCache to Riba

--- app/theme/scripts/common/services/cache.ts
+++ app/theme/scripts/common/services/cache.ts
@@ -1,19 +1,2 @@
-import createCache from "keshi";
-import type { Storage } from "keshi";
-import { get, set, keys, del, clear } from "idb-keyval";
-
-const idbStorage: Storage = {
-  get,
-  set,
-  keys,
-  del,
-  clear: () => {
-    return clear() as any;
-  },
-};
-const defaultCache = createCache({
-  // Use default memory storage on ssr
-  customStorage: window.ssr ? undefined : idbStorage,
-});
-
-export { createCache, defaultCache };
+import { idbCache, createCache } from '@ribajs/cache';
+export { createCache, idbCache as defaultCache };
--- app/theme/scripts/csr/components/index.ts
+++ app/theme/scripts/csr/components/index.ts
@@ -1,6 +1,5 @@
 export { CountUpComponent } from "./count-up/count-up.component";
 export { GyBlackboardComponent } from "./gy-blackboard/gy-blackboard.component";
-export { GyDatesComponent } from "./gy-dates/gy-dates.component";
 export { GyMensaMaxComponent } from "./gy-mensa-max/gy-mensa-max.component";
 export { GyNavDropdownComponent } from "./gy-nav-dropdown/gy-nav-dropdown.component";
 export { GyNavSlideComponent } from "./gy-nav-slide/gy-nav-slide.component";
